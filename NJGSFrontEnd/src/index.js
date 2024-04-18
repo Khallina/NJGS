ce4f20d5e22e87e0efd6df8279571513058b3331
ryanptolentino <77427201+ryanptolentino@users.noreply.github.com>
2024-04-18 14:06:16 -0700

    Delete NJGSFrontEnd directory

--- NJGSFrontEnd/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MyApp from './MyApp'
-import './index.css'
-import { CookiesProvider } from 'react-cookie'
-
-ReactDOM.render(<CookiesProvider><MyApp /></CookiesProvider>, document.getElementById('root'))
